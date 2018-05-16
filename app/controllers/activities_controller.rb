a97f207c1fa12042e032be4a3dd84c1d94acf32d
oscar ortiz <ortizg.oscar@gmail.com>
2018-05-16 18:41:28 -0300

    crud working for tour stores

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,54 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  def index
+    @activites = Activity.all
+  end
+
+
   def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(set_params)
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def index
+  def update
+    @activity.update(set_params)
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render :edit
+    end
   end
+
+
 
   def show
   end
+
+  def destroy
+    @activity.destroy
+    redirect_to tour_stores_path
+  end
+
+  private
+
+  def set_params
+    params.require(:activity).permit(:name, :description, :max_spots, :departure_location
+                              :date, :price, :tour_store_id)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
 end
--- app/controllers/tour_stores_controller.rb
+++ app/controllers/tour_stores_controller.rb
@@ -1,13 +1,53 @@
 class TourStoresController < ApplicationController
+  before_action :set_tour_store, only: [:edit, :update, :show, :destroy]
+  def index
+    @tour_stores = TourStore.all
+  end
+
+
   def new
+    @tour_store = TourStore.new
+  end
+
+  def create
+    @tour_store = TourStore.new(set_params)
+    @tour_store.user = current_user
+    if @tour_store.save
+      redirect_to tour_store_path(@tour_store)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def index
+  def update
+    @tour_store.update(set_params)
+    if @tour_store.save
+      redirect_to tour_store_path(@tour_store)
+    else
+      render :edit
+    end
   end
 
   def show
   end
+
+  def destroy
+    @tour_store.destroy
+  end
+
+
+  private
+
+  def set_params
+    params.require(:tour_store).permit(:address, :phone, :website, :name, :description,
+                                      :business_tax_id, :regulator_id, :logo, :user_id, :photo, :photo_cache)
+  end
+
+  def set_tour_store
+    @tour_store = TourStore.find(params[:id])
+  end
+
 end
