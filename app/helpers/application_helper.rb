d3d046854eb2de01430cdaa0de47d6839d8b2a94
oscar ortiz <ortizg.oscar@gmail.com>
2018-05-17 21:51:13 -0300

    refactored view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+require_relative 'path_helper'
 module ApplicationHelper
+
+  def tourcontrole_page?
+    (@tour_store.nil? && @activity.nil?)  ||
+    page?( 'tour_stores#dashboard, tour_stores#new, tour_stores#edit') ||
+    page?('pages')
+  end
+
 end
