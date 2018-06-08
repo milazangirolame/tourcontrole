5f9e0063a679714ab0c0400edba8c220bd699493
oscar ortiz <ortizg.oscar@gmail.com>
2018-06-08 10:50:52 -0300

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,17 @@ Rails.application.routes.draw do
     get 'tours', to: 'tour_stores#tours'
     get 'users', to: 'tour_stores#users'
     get 'events', to: 'tour_stores#events'
-    resources :orders, only:[:create, :new]
     resources :tour_store_admins, only: [:create, :new, :destroy]
     resources :activities, only: [:edit, :update, :new, :create] do
       get 'audit', to: 'activities#audit', as: :audit
     end
   end
   resources :tour_store_admins, only: [:edit, :update, :destroy]
-  resources :activities, only: [:destroy, :show] do
+  delete ':activity_name', to: 'activities#destroy'
+  get 'activities/:id', to: 'activities#show', as: 'activity' do
     resources :orders, only: [:create, :new]
   end
+
   resources :orders, only:[:show, :update, :edit, :destroy, :index]
   get 'terms', to: 'pages#terms'
   get 'about', to: 'pages#about'
