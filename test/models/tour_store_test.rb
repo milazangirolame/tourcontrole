7ac9ebea09a9c9b1b5a4818fd54daad50408cf7a
oscar ortiz <ortizg.oscar@gmail.com>
2018-06-11 15:45:29 -0300

    not done

--- test/models/tour_store_test.rb
+++ test/models/tour_store_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
-
 class TourStoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   test 'Creating a new store' do
     store = tour_stores(:store)
     assert_equal 'tourcontrole.com', store.website, ['Should bring up the tourcontrole.com website']
